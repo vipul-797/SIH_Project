02ae251ec1e3ff06026ca6a9918be6ba017055b6
Vipul <jainvipul807@gmail.com>
2023-12-19 19:42:39 +0530

    chnages

--- django_chatbot/django_chatbot/settings.py
+++ django_chatbot/django_chatbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wsa9k4v_goql%t8rn@q4*5flo+xnnxa%8!^p2g(4g-=py==ur)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
